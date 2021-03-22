a9f913d9c7bba6a797e7cbf7ed89c5867f6a6b02
Darylabrador <abrador.daryl@gmail.com>
2021-03-22 13:52:48 +0400

    List String as list card

--- lib/main.dart
+++ lib/main.dart
@@ -9,29 +9,36 @@ class MyApp extends StatefulWidget {
   }
 }
 
-class _MyAppState extends State <MyApp> {
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food tester'];
+
   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: ElevatedButton(onPressed: () {}, child: Text('Add product')),
-            ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food paradise')
-                ],
-              ),
-            )
-          ]),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EasyList'),
         ),
-      );
-    }
+        body: Column(children: [
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: ElevatedButton(onPressed: () {}, child: Text('Add product')),
+          ),
+          Column(
+            children: _products
+              .map((element) => Card(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset('assets/food.jpg'),
+                      Text(element)
+                    ],
+                  ),
+                ),
+              )
+            .toList()
+          ),
+        ]),
+      ),
+    );
+  }
 }
