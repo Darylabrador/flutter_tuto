13e34ca5fe12325811e7889a526dd383e1a2b995
Darylabrador <abrador.daryl@gmail.com>
2021-03-22 13:26:35 +0400

    Basic statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,36 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
-        body: Column(children: [
-          Container(
-            margin: EdgeInsets.all(10.0),
-            child: ElevatedButton(onPressed: () {}, child: Text('Add product')),
-          ),
-          Card(
-            child: Column(
-              children: <Widget>[
-                Image.asset('assets/food.jpg'),
-                Text('Food paradise')
-              ],
-            ),
-          )
-        ]),
-      ),
-    );
+  State<StatefulWidget> createState() {
+    return _MyAppState();
   }
+}
+
+class _MyAppState extends State <MyApp> {
+  @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('EasyList'),
+          ),
+          body: Column(children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: ElevatedButton(onPressed: () {}, child: Text('Add product')),
+            ),
+            Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text('Food paradise')
+                ],
+              ),
+            )
+          ]),
+        ),
+      );
+    }
 }
