ce43c2f05d570ec2a9c5f278e5393cd31930db90
Darylabrador <abrador.daryl@gmail.com>
2021-03-22 18:10:06 +0400

    Button wich change the text

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,37 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  String _mainText = 'This is the first assignment';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primarySwatch: Colors.deepOrange, 
-          accentColor: Colors.deepPurple
-      ),
+          primarySwatch: Colors.deepOrange, accentColor: Colors.deepPurple),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('EasyList'),
+          title: Text('Assignment'),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _mainText = 'this changed';             
+                });
+              }, 
+              child: Text('Change text')
+            ),
+            Text(_mainText)
+          ],
         ),
       ),
     );
