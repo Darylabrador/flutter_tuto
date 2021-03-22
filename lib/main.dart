e5849ff04fb3bb2afcc9c71b0939f5fd12585595
Darylabrador <abrador.daryl@gmail.com>
2021-03-22 11:41:37 +0400

    Cleaner code

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import "package:flutter/material.dart";
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
